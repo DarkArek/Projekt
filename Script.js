5c1d67a4f21c2e83b0a12ffb377d6e3ddbefb458
DarkArek <59264392+DarkArek@users.noreply.github.com>
2020-01-06 18:49:38 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,27 +1,27 @@
 let Talia = {
-    Karta1: [1,0,Math.random(0,1)],
-    Karta2: [1,0,Math.random(0,1)],
+    karta1: [type: 1, isFaceUp: false,orderNumber: Math.random(0,1)],
+    karta2: [type: 1, isFaceUp: false,orderNumber: Math.random(0,1)],
 
-    Karta3: [2,0,Math.random(0,1)],
-    Karta4: [2,0,Math.random(0,1)],
+    karta3: [type: 2, isFaceUp: false,orderNumber: Math.random(0,1)],
+    karta4: [type: 2, isFaceUp: false,orderNumber: Math.random(0,1)],
 
-    Karta5: [3,0,Math.random(0,1)],
-    Karta6: [3,0,Math.random(0,1)],
+    karta5: [type: 3, isFaceUp: false,orderNumber: Math.random(0,1)],
+    karta6: [type: 3, isFaceUp: false,orderNumber: Math.random(0,1)],
 
-    Karta7: [4,0,Math.random(0,1)],
-    Karta8: [4,0,Math.random(0,1)],
+    karta7: [type: 4, isFaceUp: false,orderNumber: Math.random(0,1)],
+    karta8: [type: 4, isFaceUp: false,orderNumber: Math.random(0,1)],
 
-    Karta9: [5,0,Math.random(0,1)],
-    Karta10: [5,0,Math.random(0,1)],
+    karta9: [type: 5, isFaceUp: false,orderNumber: Math.random(0,1)],
+    karta10: [type: 5, isFaceUp: false,orderNumber: Math.random(0,1)],
 
-    Karta11: [6,0,Math.random(0,1)],
-    Karta12: [6,0,Math.random(0,1)],
+    karta11: [type: 6, isFaceUp: false,orderNumber: Math.random(0,1)],
+    karta12: [type: 6, isFaceUp: false,orderNumber: Math.random(0,1)],
 
-    Karta13: [7,0,Math.random(0,1)],
-    Karta14: [7,0,Math.random(0,1)],
+    karta13: [type: 7, isFaceUp: false,orderNumber: Math.random(0,1)],
+    karta14: [type: 7, isFaceUp: false,orderNumber: Math.random(0,1)],
     
-    Karta15: [8,0,Math.random(0,1)],
-    Karta16: [8,0,Math.random(0,1)],
+    karta15: [type: 8, isFaceUp: false,orderNumber: Math.random(0,1)],
+    karta16: [type: 8, isFaceUp: false,orderNumber: Math.random(0,1)],
 }
 // W Tali wartosci karty po kolej to Typ karty , Czy jest odkryta(1) czy zakryta(0), Math.Random ktory odpowiada za losowanie kolejnosci kart
-document.getElementById("karta1").innerHTML = Talia.Karta1[1]
+document.getElementById("karta1").innerHTML = Talia.Karta1[orderNumber]
